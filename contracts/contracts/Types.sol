53411d94e40f458b1d1ff0f5c96eec91e03eaf3c
elanzalaco <133013130+elanzalaco@users.noreply.github.com>
2023-11-18 12:02:05 +0300

    feat: createGame + joinGame (#2)
    * feat: createGame + joinGame
    
    * ci: fix workflow
    
    * ci: fix workflow
    
    * ci: fix workflow #3
    
    * fix: remove workflow
    
    * feat: get randomNumber + getBlock helpers
    
    * feat: submitPuzzle
    
    * feat: ifGameNotFinished
    
    * feat: submitPuzzle selecting puzzle and preventing double submission
    
    * feat: resolveGame + formatting

---
+++ contracts/contracts/Types.sol
@@ -0,0 +1,61 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+struct Game {
+    Player player1;
+    Player player2;
+    address puzzleSet;
+    uint8 interval;
+    uint16 numberOfRounds;
+    uint72 startingBlock;
+    uint256 stake;
+}
+
+struct Proof {
+    uint256[2] a;
+    uint256[2][2] b;
+    uint256[2] c;
+}
+
+struct Puzzle {
+    bytes32 startingGrid;
+    bytes32 finalGrid;
+    uint8[] availableFunctions;
+}
+
+struct Player {
+    address address_;
+    uint16 score;
+    uint72 totalBlocks; // for tie breaks
+}
+
+interface IZKube {
+    // When a game is created, the blockstart is defined. Each turn must be made within a blockinterval no greater than 256 blocks.
+    function createGame(address puzzleSet, uint8 interval, uint16 numberOfTurns)
+        external
+        payable
+        returns (uint256 id);
+
+    function joinGame(uint256 id) external payable;
+
+    // The selectPuzzle view function uses previous block.hash to select the same puzzle for both players deterministically
+    function selectPuzzle(uint256 id)
+        external
+        view
+        returns (uint256 roundBlockNumber, Game memory game, Puzzle memory puzzle);
+
+    // check is player and verify proof, revert if not valid proof.
+    function submitPuzzle(uint256 id, uint256[3] memory publicSignals, Proof calldata proof) external;
+}
+
+// Please note that you should adjust the length of the inputs
+interface IZKubeVerifier {
+    function verifyProof(uint256[2] memory a, uint256[2][2] memory b, uint256[2] memory c, uint256[3] memory input)
+        external
+        view
+        returns (bool r);
+}
+
+interface IZKubePuzzleSet {
+    function getPuzzle(uint256 randomNumber) external view returns (Puzzle memory puzzle);
+}
