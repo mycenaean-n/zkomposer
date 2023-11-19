652b4a59f084532cfd713c021edfa69a857c24da
Emilio Lanzalaco <elanzalaco@ebay.com>
2023-11-19 08:55:02 +0300

    tests: ZKubeVerifier

--- contracts/src/ZKubePuzzleSet.sol
+++ contracts/src/ZKubePuzzleSet.sol
@@ -1,37 +1,35 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { Proof, IZKube, IZKubeVerifier, Puzzle, Game, IZKubePuzzleSet, Player } from "./Types.sol";
-import { ERC721 } from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import { Ownable } from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {Proof, IZKube, IZKubeVerifier, Puzzle, Game, IZKubePuzzleSet, Player} from "./Types.sol";
+import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract ZKubePuzzleSet is IZKubePuzzleSet, ERC721, Ownable {
-  uint256 public numberOfPuzzles;
+    uint256 public numberOfPuzzles;
 
-  mapping(uint256 => Puzzle) public puzzles;
+    mapping(uint256 => Puzzle) public puzzles;
 
-  string public baseUri;
+    string public baseUri;
 
-  constructor(string memory name, string memory symbol)
-    ERC721(name, symbol)
-    Ownable(msg.sender)
-  {}
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) Ownable(msg.sender) {}
 
-  function addPuzzle(Puzzle calldata puzzle) external onlyOwner {
-    uint256 puzzleId = numberOfPuzzles++;
-    puzzles[puzzleId] = puzzle;
-    _mint(msg.sender, puzzleId);
-  }
+    function addPuzzle(Puzzle calldata puzzle) external onlyOwner {
+        uint256 puzzleId = numberOfPuzzles++;
+        puzzles[puzzleId] = puzzle;
+        _mint(msg.sender, puzzleId);
+    }
 
-  function getPuzzle(uint256 randomNumber)
-    external
-    view
-    returns (Puzzle memory puzzle)
-  {
-    puzzle = puzzles[1 + (randomNumber % numberOfPuzzles)];
-  }
+    function getPuzzle(
+        uint256 randomNumber
+    ) external view returns (Puzzle memory puzzle) {
+        puzzle = puzzles[1 + (randomNumber % numberOfPuzzles)];
+    }
 
-  function _baseURI() internal view override returns (string memory) {
-    return baseUri;
-  }
+    function _baseURI() internal view override returns (string memory) {
+        return baseUri;
+    }
 }
