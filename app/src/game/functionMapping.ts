5144cefdb86d25493487474df8f89a0270c0e31b
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2023-11-19 03:40:53 +0300

    feat: added functionMapping

---
+++ app/src/game/functionMapping.ts
@@ -0,0 +1,12 @@
+const functionMapping = {
+  1: "transform(yellow, red)",
+  2: "transform(yellow, blue)",
+  3: "transform(red, yellow)",
+  4: "transform(red, blue)",
+  5: "transform(blue, red)",
+  6: "transform(blue, yellow)",
+  7: "stack(yellow)",
+  8: "stack(red)",
+  9: "stack(blue)",
+  10: "reverse()",
+};
