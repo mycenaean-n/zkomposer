2169a8dfc2379453c45dfb515d942eca57b8f90e
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2023-11-26 14:38:11 +0300

    chore: decoupled circuit logic into multiple templates

--- circuits-hardhat/hardhat.config.ts
+++ circuits-hardhat/hardhat.config.ts
@@ -19,18 +19,8 @@ export default {
     ptau: "https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau",
     circuits: [
       {
-        name: "division",
+        name: "ZKubes",
         // No protocol, so it defaults to groth16
-      },
-      {
-        name: "simple-polynomial",
-        // Generate PLONK
-        protocol: "plonk",
-      },
-      {
-        name: "hash",
-        // Explicitly generate groth16
-        protocol: "groth16",
       },
     ],
   },
