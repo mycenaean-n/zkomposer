c4598b145685466ff6ebb7ddfbaf4436e4595441
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2023-12-14 16:20:07 +0100

    test: added case for main circuit

--- circuits-hardhat/utils/circuitFunctions.ts
+++ circuits-hardhat/utils/circuitFunctions.ts
@@ -1,28 +1,29 @@
-import { Colors } from "../test/data/puzzles.types";
+import { Colors, OnOff } from "../test/data/puzzles.types";
 
 const FUNCTION_ORDER = ["TRANSFORM", "STACK", "TRANSFORMTWO"];
 
 const ARGUMENTS = {
+  EMPTY: [OnOff.Off, 0, 0],
   // STACK
-  STACK_YELLOW: [1, Colors.Yellow, 0],
-  STACK_RED: [1, Colors.Red, 0],
-  STACK_BLUE: [1, Colors.Blue, 0],
+  STACK_YELLOW: [OnOff.On, Colors.Yellow, 0],
+  STACK_RED: [OnOff.On, Colors.Red, 0],
+  STACK_BLUE: [OnOff.On, Colors.Blue, 0],
 
   // TRANSFORM
-  TRANSFORM_YELLOW_RED: [1, Colors.Yellow, Colors.Red],
-  TRANSFORM_YELLOW_BLUE: [1, Colors.Yellow, Colors.Blue],
-  TRANSFORM_RED_YELLOW: [1, Colors.Red, Colors.Yellow],
-  TRANSFORM_RED_BLUE: [1, Colors.Red, Colors.Blue],
-  TRANSFORM_BLUE_YELLOW: [1, Colors.Blue, Colors.Yellow],
-  TRANSFORM_BLUE_RED: [1, Colors.Blue, Colors.Red],
+  TRANSFORM_YELLOW_RED: [OnOff.On, Colors.Yellow, Colors.Red],
+  TRANSFORM_YELLOW_BLUE: [OnOff.On, Colors.Yellow, Colors.Blue],
+  TRANSFORM_RED_YELLOW: [OnOff.On, Colors.Red, Colors.Yellow],
+  TRANSFORM_RED_BLUE: [OnOff.On, Colors.Red, Colors.Blue],
+  TRANSFORM_BLUE_YELLOW: [OnOff.On, Colors.Blue, Colors.Yellow],
+  TRANSFORM_BLUE_RED: [OnOff.On, Colors.Blue, Colors.Red],
 
   // TRANSFORM TWO
-  TRANSFORMTWO_YELLOW_RED: [1, Colors.Yellow, Colors.Red],
-  TRANSFORMTWO_YELLOW_BLUE: [1, Colors.Yellow, Colors.Blue],
-  TRANSFORMTWO_RED_YELLOW: [1, Colors.Red, Colors.Yellow],
-  TRANSFORMTWO_RED_BLUE: [1, Colors.Red, Colors.Blue],
-  TRANSFORMTWO_BLUE_YELLOW: [1, Colors.Blue, Colors.Yellow],
-  TRANSFORMTWO_BLUE_RED: [1, Colors.Blue, Colors.Red],
+  TRANSFORMTWO_YELLOW_RED: [OnOff.On, Colors.Yellow, Colors.Red],
+  TRANSFORMTWO_YELLOW_BLUE: [OnOff.On, Colors.Yellow, Colors.Blue],
+  TRANSFORMTWO_RED_YELLOW: [OnOff.On, Colors.Red, Colors.Yellow],
+  TRANSFORMTWO_RED_BLUE: [OnOff.On, Colors.Red, Colors.Blue],
+  TRANSFORMTWO_BLUE_YELLOW: [OnOff.On, Colors.Blue, Colors.Yellow],
+  TRANSFORMTWO_BLUE_RED: [OnOff.On, Colors.Blue, Colors.Red],
 };
 
 export function functionBuilder(args: (keyof typeof ARGUMENTS)[]) {
@@ -39,7 +40,7 @@ export function functionBuilder(args: (keyof typeof ARGUMENTS)[]) {
     const func = arg.split("_")[0];
     const index = FUNCTION_ORDER.indexOf(func);
 
-    argumentsArray[i][index] = ARGUMENTS[arg];
+    if (arg !== "EMPTY") argumentsArray[i][index] = ARGUMENTS[arg];
   });
 
   return argumentsArray;
