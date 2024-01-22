5f4a9d9e98ec2f0d98812a2548d4d50751f60515
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-01-22 16:50:43 +0100

    feat: enabled proof generation on frontend

--- app/app/game/components/Scene.tsx
+++ app/app/game/components/Scene.tsx
@@ -1,58 +1,65 @@
-import { useContext, useEffect, useState } from "react";
-import { PuzzleContext } from "./Puzzle";
-import { Grid } from "./Grid";
-import { idToMutator } from "../Puzzles";
-import { Canvas } from "@react-three/fiber";
-import { Vector3 } from "three";
-import styles from "../styles/puzzle.module.scss";
+import { useContext, useEffect, useState } from 'react';
+import { PuzzleContext } from './Puzzle';
+import { Grid } from './Grid';
+import { Canvas } from '@react-three/fiber';
+import { Vector3 } from 'three';
+import styles from '../styles/puzzle.module.scss';
+import { gridMutator } from 'circuits';
 
 export function Scene() {
-	const [grids, setGrids] = useState<number[][][]>([]);
-	const { initConfig, functions } = useContext(PuzzleContext);
-	const { startingGrid, finalGrid, availableFunctions } = initConfig;
+  const [grids, setGrids] = useState<number[][][]>([]);
+  const { initConfig, functions } = useContext(PuzzleContext);
+  const {
+    initialGrid: startingGrid,
+    finalGrid,
+    availableFunctions,
+  } = initConfig;
 
-	const xGap = 10 / (availableFunctions.length + 1);
-	let xPos = -5;
-	const gridElements = grids.map((grid, index) => {
-		xPos += xGap;
-		return <Grid key={index} grid={grid} position={{ x: xPos, y: 0, z: 0 }} />;
-	});
+  const xGap = 10 / (availableFunctions.length + 1);
+  let xPos = -5;
+  const gridElements = grids.map((grid, index) => {
+    xPos += xGap;
+    return <Grid key={index} grid={grid} position={{ x: xPos, y: 0, z: 0 }} />;
+  });
 
-	useEffect(() => {
-		setGrids([]);
-		const mutatedGrids: number[][][] = [];
-		functions.chosen.forEach((functionId, index) => {
-			if (index == 0) {
-				const grid = idToMutator[functionId](startingGrid);
-				mutatedGrids.push(grid);
-			} else {
-				const grid = idToMutator[functionId](mutatedGrids[index - 1]);
-				mutatedGrids.push(grid);
-			}
-		});
-		setGrids(mutatedGrids);
-	}, [functions]);
+  useEffect(() => {
+    setGrids([]);
+    const mutatedGrids: number[][][] = [];
+    functions.chosen.forEach((funcName, index) => {
+      if (index == 0) {
+        const grid = gridMutator(startingGrid, [funcName]);
+        console.log(funcName, 'gridgridgrid');
 
-	return (
-		<div className={styles.canvasContainer}>
-			<Canvas
-				className={styles.canvas}
-				orthographic
-				camera={{
-					position: new Vector3(0.5, 0.5, 1),
-					left: -10,
-					right: 10,
-					top: 10,
-					bottom: -10,
-					zoom: 60,
-					near: -20,
-					far: 20,
-				}}>
-				<ambientLight intensity={Math.PI} />1
-				<Grid grid={startingGrid} position={{ x: -5, y: 0, z: 0 }} />
-				{gridElements}
-				<Grid grid={finalGrid} position={{ x: 5, y: 0, z: 0 }} />
-			</Canvas>
-		</div>
-	);
+        mutatedGrids.push(grid);
+      } else {
+        const grid = gridMutator(mutatedGrids[index - 1], [funcName]);
+        mutatedGrids.push(grid);
+      }
+    });
+    setGrids(mutatedGrids);
+  }, [functions]);
+
+  return (
+    <div className={styles.canvasContainer}>
+      <Canvas
+        className={styles.canvas}
+        orthographic
+        camera={{
+          position: new Vector3(0.5, 0.5, 1),
+          left: -10,
+          right: 10,
+          top: 10,
+          bottom: -10,
+          zoom: 60,
+          near: -20,
+          far: 20,
+        }}
+      >
+        <ambientLight intensity={Math.PI} />1
+        <Grid grid={startingGrid} position={{ x: -5, y: 0, z: 0 }} />
+        {gridElements}
+        <Grid grid={finalGrid} position={{ x: 5, y: 0, z: 0 }} />
+      </Canvas>
+    </div>
+  );
 }
--- app/types/Puzzle.ts
+++ app/types/Puzzle.ts
@@ -1,20 +1,21 @@
-import { Dispatch, SetStateAction } from "react";
+import { CircuitFunctions } from 'circuits/types/circuitFunctions.types';
+import { Dispatch, SetStateAction } from 'react';
 
 export enum PuzzleFunctionState {
-	remaining = "remaining",
-	chosen = "chosen",
+  remaining = 'remaining',
+  chosen = 'chosen',
 }
 
-export type PuzzleFunctions = Record<PuzzleFunctionState, number[]>;
+export type PuzzleFunctions = Record<PuzzleFunctionState, CircuitFunctions[]>;
 
 export type PuzzleContext = {
-	initConfig: PuzzleInit;
-	functions: PuzzleFunctions;
-	setFunctions: Dispatch<SetStateAction<PuzzleFunctions>>;
+  initConfig: PuzzleInit;
+  functions: PuzzleFunctions;
+  setFunctions: Dispatch<SetStateAction<PuzzleFunctions>>;
 };
 
 export type PuzzleInit = {
-	startingGrid: number[][];
-	finalGrid: number[][];
-	availableFunctions: number[];
+  initialGrid: number[][];
+  finalGrid: number[][];
+  availableFunctions: CircuitFunctions[];
 };
