0a3de5bbcf8eaabef8eec5a0f881c065d5ecbdca
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-02-06 14:38:51 +0100

    feat: 2 new mock puzzles

--- circuits/utils/exportCalldataGroth16Invoke.ts
+++ circuits/utils/exportCalldataGroth16Invoke.ts
@@ -1,14 +1,14 @@
-import path from "path";
-import { exportCalldataGroth16 } from "./exportCalldataGroth16";
-import { writeFileSync } from "fs";
+import path from 'path';
+import { exportCalldataGroth16 } from './exportCalldataGroth16';
+import { writeFileSync } from 'fs';
 
 export async function exportCalldataGroth16Invoke() {
-  const zkeyPath = path.join(__dirname, "../zk/zkube_final.zkey");
-  const wasmPath = path.join(__dirname, "../zk/zkube_js/zkube.wasm");
-  const input = require("../zk/input.json");
+  const zkeyPath = path.join(__dirname, '../zk/zkube_final.zkey');
+  const wasmPath = path.join(__dirname, '../zk/zkube_js/zkube.wasm');
+  const input = require('../zk/input.json');
 
   const data = await exportCalldataGroth16(input, wasmPath, zkeyPath);
 
-  writeFileSync("./zk/zkube_proof.json", "[" + data + "]", "utf8");
+  writeFileSync('./zk/zkube_proof.json', '[' + data + ']', 'utf8');
   process.exit();
 }
