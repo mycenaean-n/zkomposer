ad6db345cdbab5dd1b9e5a9bd0649227de995164
emiliolanzalaco <emiliolanzalaco@hotmail.com>
2024-02-08 23:34:06 +0000

    ci: add json parsing of puzzles in deploy script

--- contracts/script/DeployZKube.sol
+++ contracts/script/DeployZKube.sol
@@ -1,41 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.22;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {ZKube} from "../src/ZKube.sol";
 import {ZKubePuzzleSet} from "../src/ZKubePuzzleSet.sol";
 import {ZKubeVerifier} from "../src/ZKubeVerifier.sol";
-import {Puzzle} from "../src/Types.sol";
+import {Puzzle, PuzzleJson} from "../src/Types.sol";
 
 contract DeployZKube is Script {
-    
-    uint8[] availableFunctions1;
-    uint8[] availableFunctions2;
-    uint8[] availableFunctions3;
-    uint8[] availableFunctions4;
+    Puzzle[] public puzzles;
 
-    Puzzle public puzzle1;
-    Puzzle public puzzle2;
-    Puzzle public puzzle3;
-    Puzzle public puzzle4;
-
-    function setUp () public {
-        availableFunctions1.push(1);
-        availableFunctions1.push(7);
-        availableFunctions1.push(10);
-
-        puzzle1 = Puzzle({ startingGrid: 0x40004000400040004000400040004000, finalGrid: 0xaa00aa00aa00aa00aa00aa00aa00aa00, availableFunctions: availableFunctions1 });
+    function setUp() public {
+        string memory file = vm.readFile("./script/data/puzzles.json");
+        bytes memory json = vm.parseJson(file);
+        PuzzleJson[] memory _puzzles = abi.decode(json, (PuzzleJson[]));
+        for (uint256 i; i < _puzzles.length; i++) {
+            Puzzle memory puzzle = Puzzle({
+                startingGrid: bytes16(_puzzles[i].startingGrid),
+                finalGrid: bytes16(_puzzles[i].finalGrid),
+                availableFunctions: _puzzles[i].availableFunctions
+            });
+            puzzles.push(puzzle);
+        }
     }
 
     function run() public {
         vm.startBroadcast();
         ZKubeVerifier verifier = new ZKubeVerifier();
         new ZKube(address(verifier));
-        ZKubePuzzleSet puzzleSet = new ZKubePuzzleSet("Official ZKubePuzzleSet", "ZKPuzzle"); 
-        puzzleSet.addPuzzle(puzzle1);
-        puzzleSet.addPuzzle(puzzle2);
-        puzzleSet.addPuzzle(puzzle3);
-        puzzleSet.addPuzzle(puzzle4);
+        ZKubePuzzleSet puzzleSet = new ZKubePuzzleSet("Official ZKubePuzzleSet", "ZKPuzzle");
+        for (uint256 i; i < puzzles.length; i++) {
+            puzzleSet.addPuzzle(puzzles[i]);
+        }
         vm.stopBroadcast();
     }
 }
