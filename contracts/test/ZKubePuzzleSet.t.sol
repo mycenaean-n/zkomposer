2d6fb11da26d2de14e01b1ae1dbbe0ee9d8adcf0
emiliolanzalaco <emiliolanzalaco@hotmail.com>
2024-03-25 19:46:21 +0000

    test(ZKubePuzzleSet): addPuzzle tests

---
+++ contracts/test/ZKubePuzzleSet.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.22;
+
+import {Test, console} from "forge-std/Test.sol";
+import {ZKubePuzzleSet} from "../src/ZKubePuzzleSet.sol";
+import {Puzzle, PuzzleJson} from "../src/Types.sol";
+
+contract ZKubePuzzleSetTest is Test {
+    ZKubePuzzleSet public puzzleSet;
+    Puzzle[] public puzzles;
+
+    address public deployer = vm.addr(1);
+
+    constructor() {
+        vm.prank(deployer);
+        puzzleSet = new ZKubePuzzleSet("Test puzzle set", "ZKPuzzle");
+    }
+
+    function test_init() public {
+        assertEq(puzzleSet.owner(), deployer, "Owner should be deployer");
+        assertEq(puzzleSet.name(), "Test puzzle set", "Name incorrectly set");
+        assertEq(puzzleSet.symbol(), "ZKPuzzle", "Symbol incorrectly set");
+    }
+
+    function test_addPuzzle() public {
+        _addPuzzles();
+        assertEq(puzzleSet.numberOfPuzzles(), puzzles.length, "Puzzles should be added");
+        for (uint256 i; i < puzzles.length; i++) {
+            (bytes16 finalGrid, bytes16 startingGrid) = puzzleSet.puzzles(i);
+            assertEq(startingGrid, puzzles[i].startingGrid, "Starting grid should be the same");
+            assertEq(finalGrid, puzzles[i].finalGrid, "Final grid should be the same");
+        }
+    }
+
+    function test_fuzz_getPuzzle(uint256 randomNumber) public {
+        _addPuzzles();
+        Puzzle memory puzzle = puzzleSet.getPuzzle(randomNumber);
+        assertNotEq(puzzle.availableFunctions.length, 0, "Puzzle should have functions");
+        assertNotEq(puzzle.finalGrid, bytes16(0), "Puzzle should have final grid");
+        assertNotEq(puzzle.startingGrid, bytes16(0), "Puzzle should have starting grid");
+    }
+
+    function _addPuzzles () private {
+        string memory file = vm.readFile("./script/data/puzzles.json");
+        bytes memory json = vm.parseJson(file);
+        PuzzleJson[] memory _puzzles = abi.decode(json, (PuzzleJson[]));
+        for (uint256 i; i < _puzzles.length; i++) {
+            Puzzle memory puzzle = Puzzle({
+                startingGrid: bytes16(_puzzles[i].startingGrid),
+                finalGrid: bytes16(_puzzles[i].finalGrid),
+                availableFunctions: _puzzles[i].availableFunctions
+            });
+            puzzles.push(puzzle);
+        }
+        for (uint256 i; i < puzzles.length; i++) {
+            vm.prank(deployer);
+            console.logBytes16(puzzles[i].startingGrid);
+            console.logBytes16(puzzles[i].finalGrid);
+            puzzleSet.addPuzzle(puzzles[i]);
+        }
+    }
+}
