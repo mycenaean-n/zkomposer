258ccdf197327728829f6064da25810176d5dfc4
emiliolanzalaco <133013130+emiliolanzalaco@users.noreply.github.com>
2024-03-25 00:08:34 +0000

    Fix/contracts-bug (#41)
    * fix(contracts): getBlock bug
    
    * docs: redeploy + update contract addresses
    
    * fix(indexer): update fromBlock

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,6 +1,6 @@
 import { Address } from 'viem';
 
 export const ZKUBE_ADDRESS: Address =
-  '0xC099fEd4f3358451faE9D0c9B71E6B4C0499078A';
+  '0xa63E564bEe5Ab70aDc3929d098d89CB55E1dEb57';
 export const ZKUBE_PUZZLESET_ADDRESS: Address =
   '0x1F7Ca0c2243A2DE388Af4Fd5d40DBA513f0E2286';
