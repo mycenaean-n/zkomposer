eb4ac7ed2afd55eebbee03e547dc079ba909ecf3
emiliolanzalaco <emiliolanzalaco@hotmail.com>
2024-03-25 19:23:40 +0000

    docs: update ZKPuzzleSet address

--- app/src/config.ts
+++ app/src/config.ts
@@ -3,4 +3,4 @@ import { Address } from 'viem';
 export const ZKUBE_ADDRESS: Address =
   '0xa63E564bEe5Ab70aDc3929d098d89CB55E1dEb57';
 export const ZKUBE_PUZZLESET_ADDRESS: Address =
-  '0x1F7Ca0c2243A2DE388Af4Fd5d40DBA513f0E2286';
+  '0x1625956d2017fED96eaA53d1B1b1311eC61963B4';
