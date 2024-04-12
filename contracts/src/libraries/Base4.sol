3c8dcaee8c02963c932a6823036d58595be0bcbb
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-04-12 14:10:09 +0200

    clean-up(contracts): removed comments

--- contracts/src/libraries/Base4.sol
+++ contracts/src/libraries/Base4.sol
@@ -3,20 +3,16 @@ pragma solidity 0.8.22;
 
 library Base4 {
     function hexToBase4(bytes16 input) internal pure returns (uint[] memory) {
-        uint[] memory base4Array = new uint[](64); // Maximum space to store base 4 numbers
+        uint[] memory base4Array = new uint[](64); 
         uint index = 0;
 
-        // Loop through each byte of the input
         for (uint i = 0; i < 16; i++) {
-            // Extract high and low nibbles
             uint high = (uint8(input[i]) >> 4) & 0x0F;
             uint low = uint8(input[i]) & 0x0F;
 
-            // Convert high nibble to base 4
             uint[] memory highBase4 = digitToBase4(high);
             uint[] memory lowBase4 = digitToBase4(low);
 
-            // Append results to base4Array, ensuring order and completeness
             for (uint j = 0; j < highBase4.length; j++) {
                 base4Array[index++] = highBase4[j];
             }
@@ -29,7 +25,7 @@ library Base4 {
     }
 
     function digitToBase4(uint digit) internal pure returns (uint256[] memory) {
-        uint256[] memory results = new uint256[](2); // At most 2 digits in base 4 for a single hex digit
+        uint256[] memory results = new uint256[](2); 
         if (digit == 0) {
             results[0] = 0;
             results[1] = 0;
