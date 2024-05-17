eb946ddc8fb39f8afcdd7be5ba9fe502dd9bae75
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-05-17 14:30:49 +0200

    feat(app): added truncatted addess when logged in

--- app/src/components/ConnectButton.tsx
+++ app/src/components/ConnectButton.tsx
@@ -1,20 +1,29 @@
 'use client';
 import { usePrivy } from '@privy-io/react-auth';
+import { truncateAddress } from '../utils/truncateAddress';
 
 export function ConnectButton() {
-  const { ready, authenticated, login } = usePrivy();
+  const { ready, authenticated, login, user } = usePrivy();
 
   // Disable login when Privy is not ready or the user is already authenticated
   const disableLogin = !ready || (ready && authenticated);
 
   return (
-    // design login button with tailwindcss. it will appear on a black background
-    <button
-      className="bg-white text-black font-bold py-2 px-4 rounded "
-      disabled={disableLogin}
-      onClick={login}
-    >
-      Log in
-    </button>
+    <>
+      {!disableLogin ? (
+        <button
+          className="bg-white text-black font-bold py-2 px-4 rounded "
+          disabled={disableLogin}
+          onClick={login}
+        >
+          Log in
+        </button>
+      ) : (
+        <div className="text-white font-bold py-2 px-4 rounded ">
+          {user?.wallet &&
+            truncateAddress(user.wallet.address as `0x${string}`)}
+        </div>
+      )}
+    </>
   );
 }
