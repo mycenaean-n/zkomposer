d33f563eb1e519e52841be4dee96c0469faf31d5
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-05-31 21:37:46 +0200

    fix(app): usePrivyWalletAddress wallet updates only if `walletClientType` === 'privy'

--- app/src/hooks/usePrivyWalletAddress.ts
+++ app/src/hooks/usePrivyWalletAddress.ts
@@ -1,8 +1,15 @@
 import { usePrivy } from '@privy-io/react-auth';
+import { useEffect, useState } from 'react';
 import { Address } from 'viem';
 
 export const usePrivyWalletAddress = () => {
   const { user } = usePrivy();
+  const [address, setAddress] = useState<Address>();
 
-  return user?.wallet?.address as Address;
+  useEffect(() => {
+    if (user?.wallet?.walletClientType === 'privy')
+      setAddress(user?.wallet?.address as Address);
+  }, [user?.wallet?.address]);
+
+  return address;
 };
