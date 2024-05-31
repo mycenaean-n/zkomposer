eb3be1563ed3450669cf01f6bb75e2529f30cbd8
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-05-31 22:07:53 +0200

    refactor(app): moved SinglePlayerGame into page

--- app/app/puzzle/[id]/page.tsx
+++ app/app/puzzle/[id]/page.tsx
@@ -1,11 +1,40 @@
 'use client';
-import { usePrivy } from '@privy-io/react-auth';
-import { SinglePlayerGame } from '../../../src/components/game/SinglePlayerGame';
 import { LoginCTA } from '../../../src/components/wallet/LoginCTA';
 import { usePrivyWalletAddress } from '../../../src/hooks/usePrivyWalletAddress';
+import { useBlockNumber } from '../../../src/hooks/useBlockNumber';
+import { usePuzzleData } from '../../../src/hooks/usePuzzleData';
+import { PuzzleMemoized } from '../../../src/components/game/puzzle/Puzzle';
 
-export default function Page({ params }: { params: { id: string } }) {
+function LoadingState(text: string) {
+  return (
+    <div className="flex flex-grow justify-center items-center text-align-center w-screen h-full text-2xl">
+      <h1>{text}</h1>
+    </div>
+  );
+}
+
+export default function Page({ params: { id } }: { params: { id: string } }) {
+  const blockNumber = useBlockNumber();
   const address = usePrivyWalletAddress();
+  const { puzzle, loading } = usePuzzleData(id);
 
-  return address ? <SinglePlayerGame id={params.id} /> : <LoginCTA />;
+  if (loading) {
+    return LoadingState('Loading puzzle...');
+  }
+
+  if (!puzzle && !loading) {
+    return LoadingState('Puzzle not found');
+  }
+
+  if (!address && blockNumber) {
+    return <LoginCTA />;
+  }
+
+  return (
+    <>
+      {puzzle && (
+        <PuzzleMemoized initConfig={puzzle} id={id} gameMode="singleplayer" />
+      )}
+    </>
+  );
 }
--- app/src/hooks/usePuzzleData.ts
+++ app/src/hooks/usePuzzleData.ts
@@ -3,48 +3,21 @@ import { useEffect, useState } from 'react';
 import { Puzzle } from '../types/Puzzle';
 import { usePuzzleSetContract } from './useContract';
 
-export const useFetchPuzzleByPuzzleId = (
+export const usePuzzleData = (
   puzzleId: string
-): Puzzle | undefined => {
-  // const [puzzle, setPuzzle] = useState<Puzzle>();
-  // const { getPuzzle } = usePuzzleSetContract();
+): { puzzle: Puzzle | undefined; loading: boolean } => {
+  const [puzzle, setPuzzle] = useState<Puzzle>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const { getPuzzle } = usePuzzleSetContract();
 
-  // useEffect(() => {
-  //   if (!getPuzzle) return;
-  //   getPuzzle(BigInt(puzzleId)).then((data?: Puzzle) => setPuzzle(data));
-  // }, [puzzleId, getPuzzle]);
+  useEffect(() => {
+    if (!getPuzzle) return;
+    setLoading(true);
+    getPuzzle(BigInt(puzzleId)).then((data?: Puzzle) => {
+      setPuzzle(data);
+      setLoading(false);
+    });
+  }, [puzzleId, getPuzzle]);
 
-  // console.log('puzzle', JSON.stringify(puzzle));
-  return {
-    initialGrid: [
-      [1, 1, 2, 0, 0, 0, 0, 0],
-      [1, 2, 0, 0, 0, 0, 0, 0],
-      [2, 0, 0, 0, 0, 0, 0, 0],
-      [2, 0, 0, 0, 0, 0, 0, 0],
-      [1, 2, 0, 0, 0, 0, 0, 0],
-      [1, 1, 2, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-    ],
-    finalGrid: [
-      [2, 2, 2, 0, 0, 0, 0, 0],
-      [2, 2, 0, 0, 0, 0, 0, 0],
-      [2, 0, 0, 0, 0, 0, 0, 0],
-      [2, 0, 0, 0, 0, 0, 0, 0],
-      [2, 2, 0, 0, 0, 0, 0, 0],
-      [2, 2, 2, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-    ],
-    availableFunctions: [
-      'TRANSFORM_YELLOW_RED',
-      'STACK_YELLOW',
-      'TRANSFORMTWO_YELLOW_YELLOW_RED',
-      'REJECT_YELLOW',
-      'EMPTY',
-      'EMPTY',
-      'EMPTY',
-      'EMPTY',
-    ],
-  };
+  return { puzzle, loading };
 };
