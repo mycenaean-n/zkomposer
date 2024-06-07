18b5d20bef5be6ec17703ef5da2efee12d018c54
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-06-07 16:12:38 +0200

    style(app): added function mobile design

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -1,4 +1,8 @@
 import { Colors } from 'circuits/types/circuitFunctions.types';
+import {
+  AvailableFunctions,
+  ColorsKeys,
+} from 'circuits/types/circuitFunctions.types';
 
 export function mapGrid(gridString: string): Colors[][] {
   const gridArray = Array.from(gridString);
@@ -10,4 +14,34 @@ export function mapGrid(gridString: string): Colors[][] {
   });
 
   return grid;
+}
+
+export function bgColor(color: ColorsKeys) {
+  switch (color) {
+    case 'YELLOW':
+      return 'bg-yellow-500';
+    case 'RED':
+      return 'bg-red-500';
+    case 'BLUE':
+      return 'bg-blue-500';
+    default:
+      throw Error('Not expected colour.');
+  }
+}
+
+export function getDisplayName(func: AvailableFunctions) {
+  switch (func) {
+    case 'TRANSFORM':
+      return 'transform';
+    case 'STACK':
+      return 'stack';
+    case 'TRANSFORMTWO':
+      return 'map';
+    case 'REJECT':
+      return 'reject';
+    case 'FILTER':
+      return 'filter';
+    default:
+      throw Error('Not expected function.');
+  }
 }
