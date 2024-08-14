9ba2e3939e86d480c9086db70701f0bf0ed245fd
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-08-14 22:32:09 +0200

    refactor(app): LevelAction into separate component

---
+++ app/src/components/game/puzzle/actions/LevelAction.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Tick } from './Actions';
+import { useRouter } from 'next/router';
+import { GameMode } from 'types/Game';
+
+type LevelActionProps = {
+  proofGenerationError?: string;
+  puzzleSolved: boolean;
+  id: string;
+  gameMode: GameMode;
+};
+
+export function LevelAction({
+  proofGenerationError,
+  puzzleSolved,
+  id,
+  gameMode,
+}: LevelActionProps) {
+  const router = useRouter();
+
+  return (
+    <div className="absolute -top-24 right-2 flex flex-col md:-top-32 md:right-14">
+      {proofGenerationError && (
+        <h2 className="mt-2 text-sm md:text-2xl">{proofGenerationError}</h2>
+      )}
+      {puzzleSolved && (
+        <>
+          <div className="flex">
+            <Tick />
+            <h2 className="mt-2 text-sm md:text-2xl">Puzzle Solved</h2>
+          </div>
+          {gameMode === 'singleplayer' && (
+            <button
+              onClick={() => router.push(`/puzzle/${Number(id) + 1}`)}
+              className="btn-transparent mt-2"
+            >
+              Next Level
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
