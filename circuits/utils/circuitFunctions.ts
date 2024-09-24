a7727da2347a36dbbf6783dac07ef3ebcad412fc
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-09-24 18:14:31 +0200

    refactor(circuits): refactored argumentBuilder

--- circuits/utils/circuitFunctions.ts
+++ circuits/utils/circuitFunctions.ts
@@ -6,32 +6,27 @@ import {
 } from '../types/circuitFunctions.types';
 import { parseCircuitArguments } from './parseCircuitArguments';
 
+function mapColor(color: string): Colors {
+  switch (color) {
+    case 'YELLOW':
+      return COLORS.YELLOW;
+    case 'RED':
+      return COLORS.RED;
+    case 'BLUE':
+      return COLORS.BLUE;
+    default:
+      return COLORS.WHITE;
+  }
+}
+
 export function argumentBuilder(
   arg: CircuitFunctions
 ): [OnOff.On, Colors, Colors, Colors] {
   const { colorOne, colorTwo, colorThree } = parseCircuitArguments(arg);
   return [
     OnOff.On,
-    colorOne === 'YELLOW'
-      ? COLORS.YELLOW
-      : colorOne === 'RED'
-        ? COLORS.RED
-        : colorOne === 'BLUE'
-          ? COLORS.BLUE
-          : COLORS.WHITE,
-    colorTwo === 'YELLOW'
-      ? COLORS.YELLOW
-      : colorTwo === 'RED'
-        ? COLORS.RED
-        : colorTwo === 'BLUE'
-          ? COLORS.BLUE
-          : COLORS.WHITE,
-    colorThree === 'YELLOW'
-      ? COLORS.YELLOW
-      : colorThree === 'RED'
-        ? COLORS.RED
-        : colorThree === 'BLUE'
-          ? COLORS.BLUE
-          : COLORS.WHITE,
+    mapColor(colorOne),
+    mapColor(colorTwo),
+    mapColor(colorThree),
   ];
 }
