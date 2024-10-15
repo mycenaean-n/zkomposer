3dd4b4dc9be8e93a371c0f508591f983614d8090
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-10-15 11:03:56 +0200

    chore(contracts): events cleanup

--- contracts/src/Errors.sol
+++ contracts/src/Errors.sol
@@ -1,16 +1,5 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-error GameStarted();
-error GameNotStarted();
-error GameInactive();
-error IntervalTooBig();
-error JoiningYourOwnGame();
-error StakeNotMet();
 error InvalidProof();
-error NotValidPlayer();
-error GameFinished();
-error GameNotFinished();
 error AlreadySubmitted();
-error InvalidSolver();
-error InvalidAvailableFunction();
