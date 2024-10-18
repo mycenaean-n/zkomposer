244dca89cae2176e94b176abb351ea2dfa5cd149
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-10-18 20:12:47 +0200

    refactor(app): removed unused styles from layout

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,15 +1,9 @@
-import { Roboto } from 'next/font/google';
 import { Header } from '../src/components/header/Header';
 import { PageFooter } from '../src/components/PageFooter';
 import { BlockProvider } from '../src/context/BlockContext';
 import { ApolloClientProvider } from '../src/providers/ApolloClientProvider';
 import { Web3Provider } from '../src/providers/Web3Provider';
 import './globals.css';
-
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-});
 
 export default function RootLayout({
   children,
