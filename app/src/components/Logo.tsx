de46822fb8328273156cf10b0fcb186fbf045c5c
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-10-26 21:11:48 +0200

    refactor: removed Link from Logo

--- app/src/components/Logo.tsx
+++ app/src/components/Logo.tsx
@@ -1,17 +1,7 @@
 'use client';
-import Link from 'next/link';
 import Image from 'next/image';
-import React from 'react';
 import logo from '../assets/zKubeLogo.svg';
 
 export function Logo() {
-  return (
-    <Link href="/">
-      <Image
-        src={logo}
-        alt="logo"
-        className="mr-4 h-8 w-auto cursor-pointer md:h-14"
-      />
-    </Link>
-  );
+  return <Image src={logo} alt="logo" className="mr-4 h-8 w-auto md:h-14" />;
 }
