5e7a4465e56bc101996dd2187776efc843efc7f0
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-10-30 22:24:02 +0100

    feat(app): added useUserLeaderboard hook

---
+++ app/src/hooks/useUserLeaderboard.ts
@@ -0,0 +1,42 @@
+import { gql, useQuery } from '@apollo/client';
+import { Address, checksumAddress } from 'viem';
+
+const USER_LEADERBOARD = gql`
+  query UserLeaderboard($puzzleSet: String!, $take: Int!) {
+    users(orderBy: totalSolved_DESC_NULLS_LAST, limit: $take) {
+      id
+      totalSolved
+      solutions(where: { puzzleSet_eq: $puzzleSet }) {
+        id
+      }
+    }
+  }
+`;
+
+export function useUserLeaderboard(puzzleSet: Address, take: number = 5) {
+  const { data, loading, error } = useQuery<{
+    users: {
+      totalSolved: number;
+      id: Address;
+      solutions: {
+        id: string;
+      }[];
+    }[];
+  }>(USER_LEADERBOARD, {
+    variables: {
+      puzzleSet: checksumAddress(puzzleSet),
+      take,
+    },
+  });
+
+  const parsedUsers = data?.users.map((user) => ({
+    ...user,
+    totalSolvedInPuzzleSet: user.solutions.length,
+  }));
+
+  return {
+    users: parsedUsers || [],
+    loading,
+    error,
+  };
+}
