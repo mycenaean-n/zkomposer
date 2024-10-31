80116bca07af7e32c82507a6e510f4ac4052a17c
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-10-31 14:08:55 +0100

    feat(app): added useReadContractPuzzleSet hook

---
+++ app/src/hooks/fetching/useReadContract.ts
@@ -0,0 +1,21 @@
+import { ContractFunctionArgs, ContractFunctionName } from 'viem';
+import { useReadContract, UseReadContractParameters } from 'wagmi';
+import { abi as PUZZLESET_ABI } from '../../abis/zKubePuzzleSet';
+import { ZKUBE_PUZZLESET_ADDRESS } from '../../config';
+
+type PuzzleSetAbi = typeof PUZZLESET_ABI;
+
+export const useReadContractPuzzleSet = <
+  funName extends ContractFunctionName<PuzzleSetAbi, 'view' | 'pure'>,
+  argsT extends ContractFunctionArgs<PuzzleSetAbi, 'view' | 'pure', funName>,
+>(
+  functionName: funName,
+  args: argsT
+) => {
+  return useReadContract<PuzzleSetAbi, funName, argsT>({
+    abi: PUZZLESET_ABI,
+    address: ZKUBE_PUZZLESET_ADDRESS,
+    functionName,
+    args,
+  } as UseReadContractParameters<PuzzleSetAbi, funName, argsT>);
+};
