90ecd9c212036de1038170ef1fa5b7ce6247374b
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-11-01 14:01:09 +0100

    refactor(app): usePuzzleData adjusted for wagmi

--- app/src/hooks/fetching/usePuzzleData.ts
+++ app/src/hooks/fetching/usePuzzleData.ts
@@ -1,52 +1,29 @@
 'use client';
 import { circuitFunctionsArray } from 'circuits/types/circuitFunctions.types';
 import { convertPuzzleToBase4FromHex } from 'circuits/utils/contracts/hexConversion';
-import { useEffect, useState } from 'react';
-import { ContractFetchReturnType } from '../../types/Hooks';
-import { OnChainPuzzle, Puzzle } from '../../types/Puzzle';
+import { useMemo } from 'react';
 import { mapGrid } from '../../utils';
-import { useZkubePuzzleSetContract } from '../useContract';
+import { useReadContractPuzzleSet } from './useReadContract';
 
-export const usePuzzleData = (
-  puzzleId: string
-): ContractFetchReturnType<Puzzle> => {
-  const [response, setResponse] = useState<ContractFetchReturnType<Puzzle>>({
-    loading: true,
-    success: false,
-    error: 'Loading puzzle...',
-  });
-  const zKubePuzzleSetContract = useZkubePuzzleSetContract();
+export const usePuzzleData = (puzzleId: string) => {
+  const { data, isLoading, error } = useReadContractPuzzleSet('getPuzzle', [
+    BigInt(puzzleId),
+  ]);
 
-  useEffect(() => {
-    if (!zKubePuzzleSetContract || !puzzleId) {
-      setResponse({ loading: false, success: false, error: 'Missing data' });
-      return;
+  const parsedData = useMemo(() => {
+    if (!data) {
+      return undefined;
     }
 
-    zKubePuzzleSetContract?.read
-      .getPuzzle([BigInt(puzzleId)])
-      .then((data) => {
-        const base4Puzzle = convertPuzzleToBase4FromHex(data as OnChainPuzzle);
-        setResponse({
-          loading: false,
-          success: true,
-          data: {
-            initialGrid: mapGrid(base4Puzzle.startingGrid, 4),
-            finalGrid: mapGrid(base4Puzzle.finalGrid, 4),
-            availableFunctions: base4Puzzle.availableFunctions.map(
-              (functionId) => circuitFunctionsArray[functionId]
-            ),
-          },
-        });
-      })
-      .catch((error) => {
-        setResponse({
-          loading: false,
-          success: false,
-          error: (error as Error).message,
-        });
-      });
-  }, [puzzleId, zKubePuzzleSetContract?.address]);
+    const base4Puzzle = convertPuzzleToBase4FromHex(data);
+    return {
+      initialGrid: mapGrid(base4Puzzle.startingGrid),
+      finalGrid: mapGrid(base4Puzzle.finalGrid),
+      availableFunctions: base4Puzzle.availableFunctions.map(
+        (functionId) => circuitFunctionsArray[functionId]
+      ),
+    };
+  }, [data]);
 
-  return response;
+  return { data: parsedData, isLoading, error };
 };
