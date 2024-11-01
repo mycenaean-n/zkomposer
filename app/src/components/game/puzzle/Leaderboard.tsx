0b140bbf72bbf8d3a1326431dde2c99567a3caf7
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-11-01 14:12:49 +0100

    feat(app): added Leaderboard component

---
+++ app/src/components/game/puzzle/Leaderboard.tsx
@@ -0,0 +1,66 @@
+import clsx from 'clsx';
+import { Address } from 'viem';
+import { usePrivyWalletAddress } from '../../../hooks/usePrivyWalletAddress';
+import { useUserLeaderboard } from '../../../hooks/useUserLeaderboard';
+import { truncateAddress } from '../../../utils/truncateAddress';
+
+type LeaderboardProps = {
+  puzzleSet: Address;
+  puzzleId: string;
+};
+
+export function Leaderboard({ puzzleSet, puzzleId }: LeaderboardProps) {
+  const { users } = useUserLeaderboard(puzzleSet, 20);
+  const address = usePrivyWalletAddress();
+
+  return (
+    <div className="w-full overflow-y-auto">
+      <table className="w-full table-fixed border-collapse rounded-lg bg-white shadow-sm">
+        <caption className="mb-2 text-start text-lg font-semibold">
+          Leaderboard
+        </caption>
+        <thead className="bg-gray-50">
+          <tr className="border-b border-gray-200">
+            <th className="w-1/12 px-4 py-2 text-center text-sm font-semibold text-gray-600">
+              #
+            </th>
+            <th className="w-3/12 px-4 py-2 text-center text-sm font-semibold text-gray-600">
+              Who
+            </th>
+            <th className="w-2/12 px-4 py-2 text-center text-sm font-semibold text-gray-600">
+              Solved
+            </th>
+            <th className="w-2/12 px-4 py-2 text-center text-sm font-semibold text-gray-600">
+              Current
+            </th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-200">
+          {Array.from({ length: 20 }, (_, i) => {
+            const user = users[i];
+            return (
+              <tr key={user?.id} className={clsx('h-8 transition-colors')}>
+                <td className="px-4 py-2 text-center text-sm text-gray-500">
+                  {user && i + 1}
+                </td>
+                <td className="px-4 py-2 text-center text-sm font-medium text-gray-900">
+                  {user?.id && truncateAddress(user.id)}
+                </td>
+                <td className="px-4 py-2 text-center text-sm text-gray-500">
+                  {user?.totalSolvedInPuzzleSet}
+                </td>
+                <td className="px-4 py-2 text-center text-sm text-gray-500">
+                  {
+                    !!user?.solutions.some(
+                      (s) => s.id.toUpperCase() === puzzleId.toUpperCase()
+                    )
+                  }
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
