e7e2676f9a041e96ccb3e9ac8f31a60236fd8012
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-11-01 14:05:29 +0100

    refactor(app): removed useBLockNumber & useCurrentRound & useGameSubscription

--- app/src/hooks/useBlockNumber.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { BlockContext } from '../context/BlockContext';
-
-export const useBlockNumber = () => {
-  const blockNumber = useContext(BlockContext);
-  return blockNumber;
-};
