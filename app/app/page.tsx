9cfd2434179faf6a7b652246c3395a77f986b525
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-11-06 18:17:05 +0100

    feat(app): added middleware for redrects

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,14 +1,3 @@
-'use client';
-import { redirect, useParams } from 'next/navigation';
-import { ZKUBE_PUZZLESET_ADDRESS } from '../src/config';
-
 export default function Home() {
-  // const [id] = useLocalStorage('puzzleId', '0');
-  const params = useParams();
-  const id = params?.id ? String(params.id) : '0';
-  const puzzleSet = params?.puzzleSet
-    ? String(params.puzzleSet)
-    : ZKUBE_PUZZLESET_ADDRESS;
-
-  redirect(`/puzzle/${puzzleSet}/${id}`);
+  return null;
 }
---
+++ app/middleware.ts
@@ -0,0 +1,19 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import { ZKUBE_PUZZLESET_ADDRESS } from './src/config';
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith('/puzzle/')) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(
+    new URL(`/puzzle/${ZKUBE_PUZZLESET_ADDRESS}/0`, request.url)
+  );
+}
+
+export const config = {
+  matcher: ['/((?!puzzle/|api/|_next/|favicon.ico).*)'],
+};
