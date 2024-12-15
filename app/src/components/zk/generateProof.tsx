0f4d99135169e7b3ec86bf3065675715a5df2445
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2024-12-15 19:23:18 +0100

    fix(app): Providers ordering

--- app/src/components/zk/generateProof.tsx
+++ app/src/components/zk/generateProof.tsx
@@ -1,21 +1,20 @@
+import { useProof } from '../../context/ProofContext';
 import { LevelAction } from '../game/actions/LevelAction';
 import { Button } from '../ui/Button';
-import { useProofGeneration } from './hooks/useProofGeneration';
 
 export function GenerateProof() {
   const {
     loading: generateProofLoading,
     generateAndVerifyProof,
     error,
-  } = useProofGeneration();
+  } = useProof();
 
   return (
     <div className="relative mb-2 h-full">
       <Button
         className="min-h-8 w-full border border-black p-1 text-white shadow-md shadow-black/50 transition-transform hover:-translate-y-px hover:shadow-lg hover:shadow-black/50"
         variant="primary"
-        disabled={!!error}
-        loading={generateProofLoading}
+        disabled={!!error || generateProofLoading}
         onClick={generateAndVerifyProof}
         type="button"
         rounded
