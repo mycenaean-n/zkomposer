ee4f3ca40ea10291f8b6a3c5e9ef7aaa073acb5d
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2025-02-01 22:45:29 +0100

    refactor(app): revert Leaderboard design

--- app/src/components/game/sidepanel/Leaderboard.tsx
+++ app/src/components/game/sidepanel/Leaderboard.tsx
@@ -1,39 +1,70 @@
-import { Address } from 'viem';
+'use client';
+import clsx from 'clsx';
 import { usePrivyWalletAddress } from '../../../hooks/privy/usePrivyWalletAddress';
+import { useRouteParams } from '../../../hooks/useRouteChange';
 import { useUserLeaderboard } from '../../../hooks/useUserLeaderboard';
+import { hasSubmittedPuzzle } from '../../../utils/hasSubmittedPuzzle';
 import { truncateAddress } from '../../../utils/truncateAddress';
 
-type LeaderboardProps = {
-  puzzleSet: Address | null;
-  puzzleId: string | null;
-};
-
-export function Leaderboard({ puzzleSet, puzzleId }: LeaderboardProps) {
-  const { users, loading } = useUserLeaderboard(puzzleSet, 5);
+export function Leaderboard() {
+  const { id, puzzleSet } = useRouteParams();
+  const { users } = useUserLeaderboard(puzzleSet, 20);
   const { address } = usePrivyWalletAddress();
 
   return (
-    <div className="flex flex-col gap-2">
-      <h2 className="text-lg font-semibold">Leaderboard</h2>
-      <table className="w-full">
-        <thead>
-          <tr>
-            <th className="text-left">Rank</th>
-            <th className="text-left">Address</th>
-            <th className="text-right">Solved</th>
+    <div className="w-full overflow-y-auto">
+      <table className="w-full table-fixed border-collapse rounded-lg bg-white shadow-sm">
+        <caption className="mb-2 text-start text-lg font-semibold">
+          Leaderboard
+        </caption>
+        <thead className="bg-gray-50">
+          <tr className="border-b border-gray-200">
+            <th className="w-1/12 px-4 py-2 text-center text-sm font-semibold text-gray-600">
+              #
+            </th>
+            <th className="w-3/12 px-4 py-2 text-center text-sm font-semibold text-gray-600">
+              Who
+            </th>
+            <th className="w-2/12 px-4 py-2 text-center text-sm font-semibold text-gray-600">
+              Solved
+            </th>
+            <th className="w-3/12 px-4 py-2 text-center text-sm font-semibold text-gray-600">
+              Current
+            </th>
           </tr>
         </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={user.id}>
-              <td>{index + 1}</td>
-              <td>{truncateAddress(user.id)}</td>
-              <td className="text-right">{user.totalSolvedInPuzzleSet}</td>
-            </tr>
-          ))}
+        <tbody className="divide-y divide-gray-200">
+          {Array.from({ length: 20 }, (_, i) => {
+            const user = users[i];
+            return (
+              <tr
+                key={user?.id}
+                className={clsx(
+                  'h-8 transition-colors',
+                  user && user.id === address && 'bg-gray-100'
+                )}
+              >
+                {user ? (
+                  <>
+                    <td className="px-4 py-2 text-center text-sm text-gray-500">
+                      {user && i + 1}
+                    </td>
+                    <td className="px-4 py-2 text-center text-sm font-medium text-gray-900">
+                      {user?.id && truncateAddress(user.id)}
+                    </td>
+                    <td className="px-4 py-2 text-center text-sm text-gray-500">
+                      {user?.totalSolvedInPuzzleSet}
+                    </td>
+                    <td className="px-4 py-2 text-center text-sm text-gray-500">
+                      {hasSubmittedPuzzle(user, id) ? '✅' : '❌'}
+                    </td>
+                  </>
+                ) : null}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
-      {loading && <div>Loading...</div>}
     </div>
   );
 }
