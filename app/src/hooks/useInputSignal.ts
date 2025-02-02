a722033706fd37c6c15531b63e319316f28df278
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2025-02-02 17:26:39 +0100

    refactor(app): move useInputSignals to hooks directory

--- app/src/hooks/useInputSignal.ts
+++ app/src/hooks/useInputSignal.ts
@@ -2,7 +2,7 @@ import { getCircuitFunctionIndex } from 'circuits';
 import { AVAILABLE_CIRCUITS } from 'circuits/config';
 import { useMemo, useState } from 'react';
 import { Address } from 'viem';
-import { usePuzzleContext } from '../../../context/PuzzleContext';
+import { usePuzzleContext } from '../context/PuzzleContext';
 
 export function useInputSignals(address: Address) {
   const { initConfig, functions } = usePuzzleContext();
