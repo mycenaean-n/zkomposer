ca9ae882748c54621d5e965cb51dc40edd7c1c27
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2025-05-16 16:14:46 +0200

    refactor(app): remove getPrivyWalletAddress in usePrivyWalletAddress

--- app/src/hooks/privy/usePrivyWalletAddress.ts
+++ app/src/hooks/privy/usePrivyWalletAddress.ts
@@ -1,16 +1,6 @@
 'use client';
-import { usePrivy, User } from '@privy-io/react-auth';
+import { usePrivy } from '@privy-io/react-auth';
 import { Address } from 'viem';
-
-const getPrivyWalletAddress = (user: User | null): Address | undefined => {
-  if (!user) {
-    return undefined;
-  }
-
-  if (user.wallet?.connectorType === 'embedded') {
-    return user.wallet.address as Address;
-  }
-};
 
 let prevAddress: Address | undefined;
 
