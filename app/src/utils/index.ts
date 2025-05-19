df9cf29053cb250c520fb2f0ca4b8f57d09d5dd7
0xMycenaean <147164866+0xMycenaean@users.noreply.github.com>
2025-05-19 16:14:00 +0200

    refactor(app): remove unused utility functions

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -1,27 +1,6 @@
-import { COLORS, Colors } from 'circuits/types/circuitFunctions.types';
-import { GRID_HEIGHT, GRID_WIDTH } from '../config';
-
-function isValidColor(value: number): value is Colors {
-  return Object.values(COLORS).includes(value as Colors);
-}
-
-export function mapGrid(gridString: string, padding: number = 0): Colors[][] {
-  const heightWithPadding = GRID_HEIGHT + padding;
-  const gridArray = Array.from(gridString);
-  const grid: Colors[][] = Array.from({ length: GRID_WIDTH }, () =>
-    Array.from({ length: heightWithPadding }, () => COLORS.WHITE)
-  );
-
-  gridArray.slice(0, GRID_WIDTH * GRID_HEIGHT).forEach((value, index) => {
-    const column = Math.floor(index / GRID_HEIGHT);
-    const columnIndex = index % GRID_HEIGHT;
-    if (isValidColor(Number(value))) {
-      grid[column][columnIndex] = Number(value) as Colors;
-    } else {
-      // TODO: don't throw, use zod
-      throw new Error(`Invalid color: ${value}`);
-    }
-  });
-
-  return grid;
-}
+export * from './base4toHexPuzzle';
+export * from './composePuzzleRoute';
+export * from './copyToClipboard';
+export * from './hasSubmittedPuzzle';
+export * from './mapGrid';
+export * from './truncateAddress';
